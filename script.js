7f398c4df0c7d106bb166cf110f198b01c2e00a9
insomniebref-ctrl <insomniebref@gmail.com>
2026-08-04 16:09:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 let vpnActif = false;
 
-
 function changerVPN() {
 
-    const etat = document.getElementById("etat");
-    const bouton = document.getElementById("vpnButton");
+    let etat = document.getElementById("etat");
+    let bouton = document.getElementById("vpnButton");
 
+    vpnActif = !vpnActif;
 
-    if (vpnActif === false) {
-
-        vpnActif = true;
-
-        etat.innerHTML = "🟢 Protection VPN activée";
-
-        bouton.innerHTML = "Désactiver VPN";
-
-    } else {
-
-        vpnActif = false;
-
-        etat.innerHTML = "🔴 Protection VPN désactivée";
-
-        bouton.innerHTML = "Activer VPN";
-
+    if (vpnActif) {
+        etat.textContent = "🟢 Protection VPN activée";
+        bouton.textContent = "Désactiver VPN";
+    } 
+    else {
+        etat.textContent = "🔴 Protection VPN désactivée";
+        bouton.textContent = "Activer VPN";
     }
 
 }
