2cb09081758fffe75cf4e0612b9fb34950816fb3
insomniebref-ctrl <insomniebref@gmail.com>
2026-08-04 16:19:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+let vpnActif = false;
 
+
+const bouton = document.getElementById("vpnButton");
+const etat = document.getElementById("etat");
+
+
+if (bouton) {
+
+    bouton.addEventListener("click", function() {
+
+
+        vpnActif = !vpnActif;
+
+
+        if (vpnActif) {
+
+            etat.innerHTML = "🟢 VPN activé";
+
+            bouton.innerHTML = "Désactiver VPN";
+
+            bouton.style.background = "#16a34a";
+
+
+        } else {
+
+            etat.innerHTML = "🔴 VPN désactivé";
+
+            bouton.innerHTML = "Activer VPN";
+
+            bouton.style.background = "#0284c7";
+
+        }
+
+
+    });
+
+}
